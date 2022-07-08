15618f59b8e4f6b432bc1e64ad9e9ee18594fd5e
Stepanov Denis <stepanov.d.m@yandex.ru>
2022-07-08 23:39:31 +0700

    Delete skyd and add comments

--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,10 @@
 pub mod postgresql {
     use core::panic;
 
+    /// Setting the transaction isolation level
+    /// creating tables in Postgresql.
     pub fn init_postgres() {
+        // Setting the transaction isolation level to serializable.
         match crate::psql::postgresql::set_transaction_isolation() {
             Ok(_) => info!("set_transaction_isolation(): ok"),
             Err(e) => {
@@ -10,6 +13,8 @@ pub mod postgresql {
             }
         }
 
+        // Creating avr_control_insert_table
+        // a table containing information about the operation of the generator.
         match crate::psql::postgresql::create_avr_control_insert_table() {
             Ok(_) => info!("create_avr_control_insert_table(): ok"),
             Err(e) => {
@@ -18,6 +23,9 @@ pub mod postgresql {
             }
         }
 
+        // Creating log_of_work_app_table
+        // a table containing information about
+        // the operation of the application (intended for the end user).
         match crate::psql::postgresql::create_log_of_work_app_table() {
             Ok(_) => info!("create_log_of_work_app_table(): ok"),
             Err(e) => {
@@ -26,6 +34,9 @@ pub mod postgresql {
             }
         }
 
+        // Creating winter_gaeden_table
+        // a table containing information about
+        // the operation of the automated winter garden management system.
         match crate::psql::postgresql::create_winter_garden_table() {
             Ok(_) => info!("create_winter_garden_table(): ok"),
             Err(e) => {
@@ -34,6 +45,9 @@ pub mod postgresql {
             }
         }
 
+        // Creating generator_load_table
+        // a table containing information about
+        // the load level connected to the generator.
         match crate::psql::postgresql::create_generator_load_table() {
             Ok(_) => info!("create_generator_load_table(): ok"),
             Err(e) => {
@@ -42,165 +56,11 @@ pub mod postgresql {
             }
         }
 
+        // Creating avr_event_table
+        // a table containing information about
+        // automatic reserve entry events.
         match crate::psql::postgresql::create_avr_events_table() {
             Ok(_) => info!("create_avr_events_table(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-    }
-}
-
-pub mod skyd {
-
-    pub fn init_skyd() {
-        match crate::skydb::skytable::set_f64_skydb("unix_from_sql", &0.00.to_string()) {
-            Ok(_) => info!("set unix_from_sql(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_f64_skydb("unix_from_sql_now", &0.00.to_string()) {
-            Ok(_) => info!("set unix_from_sql_now(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("plc_connect", &0.to_string()) {
-            Ok(_) => info!("set plc_connect(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("generator_faulty", &0.to_string()) {
-            Ok(_) => info!("set generator_faulty(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("mains_power_supply", &0.to_string()) {
-            Ok(_) => info!("set mains_power_supply(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("start_generator", &0.to_string()) {
-            Ok(_) => info!("set start_generator(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("generator_work", &0.to_string()) {
-            Ok(_) => info!("set generator_work(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("phyto_lighting_1", &0.to_string()) {
-            Ok(_) => info!("set phyto_lighting_1(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("phyto_lighting_2", &0.to_string()) {
-            Ok(_) => info!("set phyto_lighting_2(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("phyto_lighting_3", &0.to_string()) {
-            Ok(_) => info!("set phyto_lighting_3(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("phyto_lighting_4", &0.to_string()) {
-            Ok(_) => info!("set phyto_lighting_4(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("fan", &0.to_string()) {
-            Ok(_) => info!("set fan(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("automatic_watering_1", &0.to_string()) {
-            Ok(_) => info!("set automatic_watering_1(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("automatic_watering_2", &0.to_string()) {
-            Ok(_) => info!("set automatic_watering_2(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("automatic_watering_3", &0.to_string()) {
-            Ok(_) => info!("set automatic_watering_3(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("temperature_indoor", &0.to_string()) {
-            Ok(_) => info!("set temperature_indoor(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("humidity_indoor", &0.to_string()) {
-            Ok(_) => info!("set humidity_indoor(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        };
-
-        match crate::skydb::skytable::set_i32_skydb("illumination_indoor", &0.to_string()) {
-            Ok(_) => info!("set illumination_indoor(): ok"),
-            Err(e) => {
-                info!("{}", e);
-                panic!("{}", e)
-            }
-        }
-
-        match crate::skydb::skytable::set_i32_skydb("illumination_outdoor", &0.to_string()) {
-            Ok(_) => info!("set illumination_outdoor(): ok"),
             Err(e) => {
                 info!("{}", e);
                 panic!("{}", e)
