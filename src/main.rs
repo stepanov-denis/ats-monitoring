321d6d9351bbdb3b376f440566915cf317a0c551
Stepanov Denis <stepanov.d.m@yandex.ru>
2022-07-08 23:39:44 +0700

    Update main

--- src/main.rs
+++ src/main.rs
@@ -22,63 +22,58 @@ mod write_data_to_ram;
 
 /// Application workflows.
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    // Initializing a Logger.
     let mut builder = Builder::from_default_env();
     builder.target(Target::Stdout);
     builder.init();
 
     info!("starting up ats-monitoring app");
-    info!("please wait...");
-    // println!("{}", crate::psql::postgresql::db_connect());
     info!("init postresql");
+
+    // Set the transaction isolation level and
+    // create tables in Postgresql.
     init::postgresql::init_postgres();
 
-    info!("init skyd");
-    init::skyd::init_skyd();
-
-    let _write_data_to_ram_spawn = thread::spawn(|| loop {
-        info!("starting up write_data_to_ram_spawn");
-        write_data_to_ram::write::from_psql_to_skyd();
-        thread::sleep(Duration::from_millis(1000));
-    });
-
-    let _modbus_ats_spawn = thread::spawn(|| loop {
+    // Run polling of the automatic reserve input.
+    let _modbus_ats_thread = thread::spawn(|| loop {
         info!("starting up modbus_ats_spawn");
         modbus_ats::avr_control::avr_control();
         thread::sleep(Duration::from_millis(1000));
     });
 
-    let _modbus_winter_garden_spawn = thread::spawn(|| loop {
+    // Run polling of the automatic winter garden management system.
+    let _modbus_winter_garden_thread = thread::spawn(|| loop {
         info!("starting up modbus_winter_garden_spawn");
-        if modbus_winter_garden::winter_garden::winter_garden_insert().is_ok() {
-            info!("winter_garden_insert(): ok");
-        } else {
-            info!("winter_garden_insert(): error");
+        match modbus_winter_garden::winter_garden::winter_garden_insert() {
+            Ok(_) => info!("winter_garden_insert(): ok"),
+            Err(e) => info!("{}", e),
         }
         thread::sleep(Duration::from_millis(1000));
     });
 
-    let _generator_monitoring_spawn = thread::spawn(|| loop {
+    // Run the monitoring of the generator.
+    let _generator_monitoring_thread = thread::spawn(|| loop {
         info!("starting up generator_monitoring_spawn");
-        if generator_monitoring::generator::generator_state().is_ok() {
-            info!("generator_state(): ok");
-        } else {
-            info!("generator_state(): error");
+        match generator_monitoring::generator::generator_state() {
+            Ok(_) => info!("generator_state(): ok"),
+            Err(e) => info!("{}", e),
         }
         thread::sleep(Duration::from_millis(1000));
     });
 
+    // Run Telegram-bot.
     let _ats_monitoring_bot = thread::spawn(|| loop {
         info!("starting up ats_monitoring_bot");
         telegram::bot::bot_commands();
         thread::sleep(Duration::from_millis(1))
     });
 
+    // Run the monitoring of the automatic reserve input.
     loop {
         info!("starting up power_supply_monitoring_spawn");
-        if power_supply_monitoring::power_supply::ats_state().is_ok() {
-            info!("ats_state(): ok");
-        } else {
-            info!("ats_state(): error");
+        match power_supply_monitoring::power_supply::ats_state() {
+            Ok(_) => info!("ats_state(): ok"),
+            Err(e) => info!("{}", e),
         }
         thread::sleep(Duration::from_millis(1000));
     }
