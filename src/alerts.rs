8b9260fab48b39dcde47021258d2cc022ae6d614
Stepanov Denis <stepanov.d.m@yandex.ru>
2022-07-08 23:38:00 +0700

    Update alerts

--- src/alerts.rs
+++ src/alerts.rs
@@ -1,14 +1,9 @@
-pub mod gateway {
-
+pub mod sms_gateway {
+    /// Returns the uri for the http post request
+    /// for the sms gateway API to send an sms message.
     pub fn sms_message(s: &str) -> Option<String> {
         let mut message = crate::read_env::env::read("GATEWAY_STR_CONNECTION").unwrap_or_default();
         message.push_str(&crate::read_env::env::read(s).unwrap_or_default());
         Some(String::from(message))
-    }
-}
-
-pub mod tg_bot {
-    pub fn _tg_message(s: &str) -> Option<String> {
-        Some(String::from(s))
     }
 }
